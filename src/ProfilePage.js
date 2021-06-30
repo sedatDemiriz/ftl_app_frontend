4d55784f77619b659961db7af67801eca9734a9f
sedatDemiriz <sedatdemiriz97@gmail.com>
2021-06-29 23:33:46 -0700

    Added API request and props passing to ProfilePage

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import RunsTable from './RunsTable'
 
-const ProfilePage = () => {
+const axios = require('axios')
+
+const ProfilePage = (props) => {
+
+    const [profileData, setProfileData] = useState({})
+    useEffect( () => {
+        const fetchData = async () => {
+            const response = await axios.get('http://127.0.0.1:8000/api-runs/' + props.username + '/')
+            // console.log(response.data.results)
+            setProfileData(response.data.results)
+        }
+        fetchData()
+    }, [])
+
     return(
         <div>
-            <h1>Player Name</h1>
+            <h1><span>{props.username}</span></h1>
             <div>
                 <p>Best Streak</p>
                 <p>Recent Streak</p>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // import React from 'react'
 import { render } from 'react-dom'
 import RunsTable from './RunsTable'
+import ProfilePage from './ProfilePage'
 
 const App = () => {
   return <RunsTable />
 }
 
 render(
-  <App />,
+  <ProfilePage username={'farb_clone'}/>,
   document.getElementById('root')
 )
