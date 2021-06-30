eb026ab9dfc72ca3f1a43aeef9e1f6bbc5e25e68
sedatDemiriz <sedatdemiriz97@gmail.com>
2021-06-29 22:28:49 -0700

    Functionalized App component

--- src/index.js
+++ src/index.js
@@ -2,34 +2,16 @@ import React from 'react'
 import { render } from 'react-dom'
 import RunsTable from './RunsTable'
 
-// const getData = async () => 
-// await axios.get('http://127.0.0.1:8000/api-runs/')
-// .then(
-//   function(response) {
-//     return response
-//   }
+// render(
+//   <RunsTable />,
+//   document.getElementById('root')
 // )
 
-// async function getRunsResponse() {
-//   axios.get('http://127.0.0.1:8000/api-runs/')
-//   .then(function(response) {
-//     return response.data.results
-//   })
-// }
+const App = () => {
+  return <RunsTable />
+}
 
 render(
-  <RunsTable />,
+  <App />,
   document.getElementById('root')
 )
-
-// axios.get('127.0.0.1:8000/api-runs/').then(resp => {
-//   console.log(resp.data);
-// });
-
-// async function makeGetRequest() {
-//   let res = await axios.get('/api-runs')
-//   let data = res.data
-//   console.log(data)
-// }
-
-// makeGetRequest()
