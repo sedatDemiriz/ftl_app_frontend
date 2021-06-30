9e24bcaa3e0af35fbbdde93508cc5f26f96cb0dc
sedatDemiriz <sedatdemiriz97@gmail.com>
2021-06-29 23:19:48 -0700

    Fixed some indentation

--- src/RunsTable.js
+++ src/RunsTable.js
@@ -73,28 +73,28 @@ const RunsTable = () => {
 
     return (
       <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()} >{column.render('Header')}</th>))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>{row.cells.map(cell => {return (
-                <td {...cell.getCellProps()} >
-                    {cell.render('Cell')}
-                </td>
-                )})}
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()} >{column.render('Header')}</th>))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>{row.cells.map(cell => {return (
+                  <td {...cell.getCellProps()} >
+                      {cell.render('Cell')}
+                  </td>
+                  )})}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     )
 }
 
